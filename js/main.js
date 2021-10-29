a2c2a226de9f393a6fb86b9d123df3edcc6ae9dc
fabian <fabian.froding@gmail.com>
2021-10-29 21:46:51 +0200

    impl toggle between se and gd projects

--- js/main.js
+++ js/main.js
@@ -12,18 +12,12 @@ $(document).ready(function () {
     $('nav a[href*="#"]').on('click', function () {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 1500);
+        }, 0);
     });
 
     $('#up').on('click', function () {
         $('html, body').animate({
             scrollTop: 0
-        }, 1500);
-    });
-
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: true
+        }, 0);
     });
 });
---
+++ js/project-buttons.js
@@ -0,0 +1,27 @@
+function toggleSEProjects() {
+    showSEPortfolio(true);
+    showGDPortfolio(false);
+
+    const el = document.getElementById("se-projects-btn");
+    el.style.backgroundColor = '#fd7e5744';
+    const el2 = document.getElementById("gd-projects-btn");
+    el2.style.backgroundColor = '#00000000';
+}
+
+function toggleGDProjects() {
+    showSEPortfolio(false);
+    showGDPortfolio(true);
+
+    const el = document.getElementById("gd-projects-btn");
+    el.style.backgroundColor = '#fd7e5744';
+    const el2 = document.getElementById("se-projects-btn");
+    el2.style.backgroundColor = '#00000000';
+}
+
+function showSEPortfolio(val) {
+    document.getElementById("portfolio-se").style.display = val ? "block" : "none";
+}
+
+function showGDPortfolio(val) {
+    document.getElementById("portfolio-gd").style.display = val ? "block" : "none";
+}
